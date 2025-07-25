b2024356602e05ac99617fb425c5f80861604bf6
KrishnaYamphu <krishnayamphu@gmail.com>
2025-07-25 14:52:18 +0545

    js charts

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+document.writeln("hello world");
+document.writeln("<br>welcome");
+document.bgColor = "red";
+
+document.getElementById("heading").style.color = "white";
+document.getElementById("heading").style.fontStyle = "italic";
