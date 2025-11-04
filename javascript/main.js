05ce267f0857459e2d452005c89767c1f26c2569
KrishnaYamphu <krishnayamphu@gmail.com>
2025-11-04 15:38:49 +0545

    init

--- javascript/main.js
+++
@@ -1,6 +0,0 @@
-document.writeln("hello world");
-document.writeln("<br>welcome");
-document.bgColor = "red";
-
-document.getElementById("heading").style.color = "white";
-document.getElementById("heading").style.fontStyle = "italic";
